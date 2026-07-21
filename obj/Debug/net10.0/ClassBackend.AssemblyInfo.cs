20d008bb81a4a0caa9146f5637b0a072e96d23b3
Cliorus <cliorus@CliorusdeMacBook-Air.local>
2026-07-21 21:52:07 +0800

    retry deploy after cleaning local cache

--- obj/Debug/net10.0/ClassBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClassBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56610a75447ba4aedfd7b0bc53c3852dc5d15ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
